482404db5c69685ddbb68fea37e56739013be4c5
Caner Tosuner <canertosuner@outlook.com>
2018-10-15 20:53:07 +0300

    Add project files.

---
+++ NetCoreTopshelf.Sample/HelloWorldServiceHost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NetCoreTopshelf.Sample
+{
+    public class HelloWorldServiceHost
+    {
+        public void Start()
+        {
+            Console.WriteLine("Hello World Service Started !!");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Hello World Service Stopped !!");
+        }
+    }
+}
---
+++ NetCoreTopshelf.Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using Topshelf;
+
+namespace NetCoreTopshelf.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(hostConfig =>
+            {
+                hostConfig.Service<HelloWorldServiceHost>(serviceConfig =>
+                {
+                    serviceConfig.ConstructUsing(() => new HelloWorldServiceHost());
+                    serviceConfig.WhenStarted(s => s.Start());
+                    serviceConfig.WhenStopped(s => s.Stop());
+                });
+                hostConfig.RunAsLocalSystem();
+                hostConfig.SetServiceName("Hello World Service");
+                hostConfig.SetDisplayName("Hello World Service Host");
+                hostConfig.SetDescription("Hello World Service Host using .Net Core and Topshelf.");
+            });
+        }
+    }
+}
